a66ac66119154fea571b40b8c092d327a738703c
JohnPan <s3536515@student.rmit.edu.au>
2020-02-09 12:43:52 +1100

    add scroll-behavior for html

---
+++ app.js
@@ -0,0 +1,7 @@
+document.querySelector('.navbar-toggle').addEventListener('click',function(){
+    let navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('navbar-show');
+    // console.log(navbar);
+    // navbar.style.display = "block";
+    
+})
